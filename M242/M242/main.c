a387b2e080f0d2b3016718f817414ea17bf325a0
Forever-a-noob <flurin.zeberli@gmail.com>
2022-03-15 08:48:54 +0100

    init commit

---
+++ M242/M242/main.c
@@ -0,0 +1,18 @@
+/*
+ * M242.c
+ *
+ * Created: 15.03.2022 08:48:41
+ * Author : fluri
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

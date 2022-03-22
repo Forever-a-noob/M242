ee7ac212a8ae1825da967710591eb42a95046a64
Forever-a-noob <flurin.zeberli@gmail.com>
2022-03-22 08:05:37 +0100

    Aufgabe 2

--- M242/M242/main.c
+++ M242/M242/main.c
@@ -7,12 +7,21 @@
 
 #include <avr/io.h>
 
+#define BUTTONS		PINA
+#define BUTTONS_DDR DDRA
+
+#define LEDS		PORTB
+#define LEDS_DDR	DDRB
+
 
 int main(void)
-{
+{	
+	BUTTONS_DDR = 0x00;
+	LEDS_DDR	= 0xFF;
     /* Replace with your application code */
     while (1) 
     {
+		LEDS = BUTTONS;
     }
 }
 
